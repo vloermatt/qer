e259d04e70bb2289b8b0a32b13aa7658acc9ba88
Matthew Davies <114952251+vloermatt@users.noreply.github.com>
2023-03-21 17:04:10 +0200

    refactor(build): remove build files from repo

--- apps/backend/.build/src/functions/post-confirmation/src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.printText = void 0;
-const printText = (txt) => {
-    console.log("PRINTED TEXT", txt);
-};
-exports.printText = printText;
-//# sourceMappingURL=helpers.js.map
